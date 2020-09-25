68487813476bacd0189d933211fb70329f936691
Tommy <tmahre26@gmail.com>
2020-09-25 11:49:36 -0600

    Literally everything else

--- main.cpp
+++ main.cpp
@@ -2,9 +2,29 @@
 
 int main() {
   Board * b = new Board();
+
+  int turn = 0;
+  std::string mark;
+
+  std::cout << "NEW GAME" << std::endl;
+
   b->DisplayBoard();
+  while (b->GameOver() == false) {
+    if (turn % 2 == 0) {
+      std::cout << "Player One's Turn" << std::endl;
+      mark = "X";
+    }
+    else {
+      std::cout << "Player Two's Turn" << std::endl;
+      mark = "O";
+    }
+    turn ++;
+    b->GetPlayerChoice(mark);
+    b->DisplayBoard();
+  }
 
-
+  std::cout << std::endl;
+  std::cout << "GAME OVER!" << std::endl;
 
   delete b;
   return 0;
