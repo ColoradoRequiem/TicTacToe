6fad0d762dd81ee21ad0c540490409d5f1983e37
Tommy <tmahre26@gmail.com>
2020-09-25 10:03:10 -0600

    place_Marker(int,int,string)

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -13,7 +13,7 @@ public:
 	int get_cols() const {return 3;}  // board by changing these numbers and the numbers in the arr_ field
 
 	std::string get_square_value(int r, int c); // should return Squaretype but with the funcs it relies on i might need to change some things around
-	void SetSquareValue(int r, int c, std::string value); 	// set the value of a square to the given SquareType
+	void place_Marker(int r, int c, std::string value); 	// set the value of a square to the given SquareType
 	// std::vector<Position> GetMoves(Player *p); // get the possible Positions that a Player could move to
 
 	void DisplayBoard();
@@ -45,6 +45,21 @@ void Board::DisplayBoard() {
       std::cout << "_   _   _" <<std::endl;
     }
   }
+}
+
+void Board::place_Marker(int r, int c, std::string value) {
+	if (r < 0 | r > 2 | c < 0 | c > 2 ) {
+		std::cout << "ERROR: OUT OF BOUNDS" << std::endl;
+	}
+	else {
+		if (arr_[r][c] != " ") {
+			std::cout << "ERROR: SPOT ALREADY PLAYED" << std::endl;
+		}
+		else {
+			this->arr_[r][c] = value;
+		}
+	}
+	return;
 }
 
 #endif
